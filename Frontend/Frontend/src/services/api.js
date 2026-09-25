5e74c94c16c68746ab3888aa1b298afc105ffc8f
Aniket Solanke <161963782+Aniket8023@users.noreply.github.com>
2026-09-25 12:53:54 +0530

    Update api.js

--- Frontend/Frontend/src/services/api.js
+++ Frontend/Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8081/api/v1",
+  baseURL: "https://ai-recruitment-system-55kd.onrender.com/api/v1",
 });
 
 api.interceptors.request.use(
